eba6026ad4ffa7ba72874c834413bbfb63a1126f
FortuneMal <fmalaza512@gmail.com>
2023-12-07 04:19:45 -0800

    first step

---
+++ betty.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Fortune and Katlego project!\n")
+		return (0);
+}
