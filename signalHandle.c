aeced24b32107741b4ca1af6a735f92eef666548
KatliKoma <katlegokoma1@gmail.com>
2023-12-13 21:15:25 +0200

    Simple shell 0.1

--- signalHandle.c
+++ signalHandle.c
@@ -1,9 +1,14 @@
 #include <signal.h>
 #include <stdio.h>
 
+/**
+ * handleSigInt - Display a prompt and wait for the user to type a command.
+ * @sig: The signal to exit
+ * Return: No return
+ */
 
 void handleSigInt(int sig)
 {
-	printf("Received signal %d\n", sig);
+	printf(" Received signal %d\n", sig);
 	getchar();
 }
